fd9c119d69c4637cb8d4b871d1283e0dc2ef171a
Brandon Cartwright <bcartwright16@gmail.com>
2023-08-11 11:11:52 -0600

    Add switch between player 1 & 2

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// handles when a cell is clicked
 const cells = document.querySelectorAll(".cell");
-
+let player = "Player 1";
+const playerDisplay = document.querySelector(".player h1");
+playerDisplay.innerText = player;
 for (let i = 0; i < cells.length; i++) {
     cells[i].onclick = function() {
-        if (!cells[i].innerText) {
+        if (!cells[i].innerText && player == "Player 1") {
             cells[i].innerText = "X";
+            player = "Player 2";
+            playerDisplay.innerText = player;
+        }else if (!cells[i].innerText && player == "Player 2"){
+            cells[i].innerText = "O";
+            player = "Player 1";
+            playerDisplay.innerText = player;
         }
     };
 }
