1b1cc4cb70a965988c50cccb0bc2dcda929aed7c
Brandon Cartwright <bcartwright16@gmail.com>
2023-08-12 12:28:58 -0600

    Add checkWin function

--- script.js
+++ script.js
@@ -15,19 +15,13 @@ for (let i = 0; i < cells.length; i++) {
             board[Math.floor(i/3)][i%3] = 1;
             player = "Player 2";
             playerDisplay.innerText = player;
-            console.log(i);
-            console.log(board[0]);
-            console.log(board[1]);
-            console.log(board[2]);
+            checkWin();
         }else if (!cells[i].innerText && player == "Player 2"){
             cells[i].innerText = "O";
             board[Math.floor(i/3)][i%3] = 2;
             player = "Player 1";
             playerDisplay.innerText = player;
-            console.log(i);
-            console.log(board[0]);
-            console.log(board[1]);
-            console.log(board[2]);
+            checkWin();
         }
     };
 }
@@ -41,8 +35,71 @@ function resetBoard(){
         player = "Player 1";
         playerDisplay.innerText = player;
     }
-}
+};
 
 function checkWin(){
+    let boardFilled = true;
+    //check row & coloumn win
+    for(let r = 0; r < board.length; r++){
+        counter1R = 0;
+        counter2R = 0;
+        counter1C = 0;
+        counter2C = 0;
+        for(let c = 0; c < board[r].length; c++){
+            if(board[r][c] == 1){
+                counter1R += 1;
+            }else if (board[r][c] == 2){
+                counter2R += 1;
+            }else{
+                boardFilled = false;
+            }
+            if(board[c][r] == 1){
+                counter1C += 1;
+            }else if (board[c][r] == 2){
+                counter2C += 1;
+            }
+        }
+        if(counter1R == 3 || counter1C == 3){
+            playerDisplay.innerText = "Player 1 Wins!";
+            return true;
+        }else if(counter2R == 3 || counter2C == 3){
+            playerDisplay.innerText = "Player 2 Wins!";
+            return true;
+        }
+    }
+    //check diagonals
+    counter1L = 0;
+    counter2L = 0;
+    counter1R = 0;
+    counter2R = 0;
+    c = board.length - 1
+    for(let r = 0; r < board.length; r++){
+        
+        console.log(c);
+        if(board[r][r] == 1){
+            counter1L += 1;
+        }else if(board[r][r] == 2){
+            counter2L += 1;
+        }
+        if(board[r][c] == 1){
+            counter1R += 1;
+        }else if(board[r][c] == 2){
+            counter2R += 1;
+        }
+        c = c - 1;
+        if(counter1L == 3 || counter1R == 3){
+            playerDisplay.innerText = "Player 1 Wins!";
+            return true;
+        }else if(counter2L == 3 || counter2R == 3){
+            playerDisplay.innerText = "Player 2 Wins!";
+            return true;
+        }
 
-}
+    }
+    
+
+    //check if board is filled to determine if draw
+    if(boardFilled){
+        playerDisplay.innerText = "It's a draw";
+    }
+};
